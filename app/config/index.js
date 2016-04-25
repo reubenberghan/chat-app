8a2b9f7dac8bf97a18e22504cc009d191a23f811
Reuben Berghan <reuben.berghan@gmail.com>
2016-04-25 11:57:38 +1000

    update string literals

--- app/config/index.js
+++ app/config/index.js
@@ -7,19 +7,19 @@ if (process.env.NODE_ENV === 'production') {
     let redisPassword = redisURI.auth.split(':')[1];
     
     module.exports = {
-        host: process.env.host || "",
+        host: process.env.host || '',
         dbURI: process.env.dbURI,
         sessionSecret: process.env.sessionSecret,
         fb: {
             clientID: process.env.fbClientID,
             clientSecret: process.env.fbClientSecret,
-            callbackURL: process.env.host + "/auth/facebook/callback",
+            callbackURL: process.env.host + '/auth/facebook/callback',
             profileFields: ['id', 'displayName', 'photos']
         },
         twitter: {
             consumerKey: process.env.twConsumerKey,
             consumerSecret: process.env.twConsumerSecret,
-            callbackURL: process.env.host + "/auth/twitter/callback",
+            callbackURL: process.env.host + '/auth/twitter/callback',
             profileFields: ['id', 'displayName', 'photos']
         },
         redis: {
